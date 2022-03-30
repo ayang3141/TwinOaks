6d5a1540d7e99cb4a4c77a1ab6ee246ab7fb7a76
Eren56 <74675541+Eren56@users.noreply.github.com>
2022-03-29 15:56:02 -1000

    Added comments

--- searchScript.js
+++ searchScript.js
@@ -1,16 +1,15 @@
 window.onload=function main()
 {
-  console.log("TEMP")
-  var key =  "AIzaSyDxSKKcB5nkA3tNiLlNKfxVjZO_lHZLrWI";
+  var key =  "AIzaSyDxSKKcB5nkA3tNiLlNKfxVjZO_lHZLrWI"; //API Key
 
-  var cx = "c7b3e921ea64eca47";
+  var cx = "c7b3e921ea64eca47";//Identifier for our specific search engine
 
-  var q = sessionStorage.getItem("searchQuery")
-  console.log(q)
+  var q = sessionStorage.getItem("searchQuery")//Get the search query store from crawlerStart.js
+  //console.log(q) //Log the query for debugging purposes
 
-  var url = "https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=" + cx +"&q=" + q +"&callback=hndlr";
+  var url = "https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=" + cx +"&q=" + q +"&callback=hndlr"; //Assemble URL link for the api
 
   var apiLink = document.createElement("script")
   apiLink.src = url;
-  document.head.appendChild(apiLink);
+  document.head.appendChild(apiLink); //Add the script to the script tag. Call back will go to hndler
 }
