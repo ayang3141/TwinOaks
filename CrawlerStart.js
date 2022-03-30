d55e1379421ada8cb4c0354209e200397f58ed2e
cdmoriarty <cdmoriarty@wpi.edu>
2022-03-29 14:16:02 -1000

    Index is now linked to CrawlerStart

--- CrawlerStart.js
+++ CrawlerStart.js
@@ -1,4 +1,34 @@
 //button.onclick=startSearch;
+function addword()
+{
+    var wurd=document.getElementById("keywordInput").value;
+    // var wurd = prompt("Add Key Word / Phrase Here");
+    if (wurd!="")
+    {
+        var p=document.getElementById("wordlist");
+        p.innerHTML="Here are the key words to be searched";
+        var li=document.createElement("li"); 	
+        li.innerHTML=wurd;
+        var ul=document.getElementById("theList");
+        ul.appendChild(li); 
+    }
+}
+function clearList()
+{
+    let ul = document.getElementById("theList");
+    while (ul.firstChild)
+    {
+        ul.removeChild(ul.firstChild);
+    }
+}
+
+
+document.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+        e.preventDefault();
+        return false;
+    }
+});
 
 function startSearch(){
     var keyWords = document.getElementsByTagName("li")
