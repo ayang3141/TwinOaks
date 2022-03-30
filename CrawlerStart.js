2f5b9a710dda33bba76dacc203af4006945cae28
cdmoriarty <cdmoriarty@wpi.edu>
2022-03-29 14:05:10 -1000

    Should be mostly good idk

--- CrawlerStart.js
+++ CrawlerStart.js
@@ -6,11 +6,20 @@ function startSearch(){
     var search = '';
     // Get search criteria from textboxes and checkboxes
     
+    var location=document.getElementById("location").value;
+    var years=document.getElementById("years").value;
+    var employees=document.getElementById("emps").value;
+    if(document.getElementById("npo").value=="npo"){
+
+    }
+    if(document.getElementById("highschool").value=="highschool"){
+
+    }
     // Inserting Keywords
     for(var i=0;i<keyWords.length;i++){
         search += '"'+keyWords[i]+'" ';
     }
-
+    console.log(search);
     // run google search
 
     // Switch to results page
