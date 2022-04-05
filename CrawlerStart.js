2f292a317cfc5893d46ff51910abba180e738fd9
cdmoriarty <cdmoriarty@wpi.edu>
2022-04-04 15:24:40 -1000

    stuff idk im stressed

--- CrawlerStart.js
+++ CrawlerStart.js
@@ -5,10 +5,8 @@ window.onload = function initialize() {
 function addword() {
     var wurd = document.getElementById("keywordInput").value;
     // var wurd = prompt("Add Key Word / Phrase Here");
-    sessionStorage.setItem("searchQuery", sessionStorage.getItem(searchQuery) + wurd)
+    sessionStorage.setItem("searchQuery", sessionStorage.getItem("searchQuery") + " " + wurd)
     if (wurd != "") {
-        var p = document.getElementById("wordlist");
-        p.innerHTML = "Here are the key words to be searched";
         var li = document.createElement("li");
         li.innerHTML = wurd;
         var ul = document.getElementById("theList");
@@ -33,24 +31,35 @@ document.addEventListener('keypress', function (e) {
 
 function startSearch() {
     var location = document.getElementById("location").value;
-    console.log(search);
+    console.log(sessionStorage.getItem("searchQuery"));
     // sessionStorage.setItem("searchQuery", search)
 
     // Switch to results page
     // window.location.href = "search.html";
     var numResults = document.getElementById("numResults").value;
+    console.log("checking the checkboxes");
+    var boxes = document.getElementsByClassName("box");
+    console.log(boxes.length);
+    for(var i=0;i<boxes.length;i++){
+        box = boxes[i];
+        console.log(box);
+        if(box.checked){
+            console.log(box.id);
+            sessionStorage.setItem("searchQuery", sessionStorage.getItem("searchQuery") + " " + box.id);
+        }
+    }
 
     if (parseInt(numResults) != NaN) {
         var list = document.getElementById("results");
         while (list.firstChild) {
             list.removeChild(list.firstChild);
         }
-
+        
         var key = "AIzaSyDxSKKcB5nkA3tNiLlNKfxVjZO_lHZLrWI";
 
         var cx = "c7b3e921ea64eca47";
 
-        var q = "inurl:.org" + sessionStorage.getItem("searchQuery") + location
+        var q = "inurl:.org" + sessionStorage.getItem("searchQuery") + " " + location
         console.log(q)
 
         var url = "https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=" + cx + "&q=" + q + "&callback=hndlr";
